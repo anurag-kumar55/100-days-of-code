cd1937b0ab5d33617a923580359b658f2c0cf155
anurag-kumar55 <anuragkumar082005@gmail.com>
2025-09-07 21:28:47 +0530

    hi

---
+++ day 1/second.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c,d,e;
+  float f;
+  printf("enter the first number:",a);
+  scanf("%d",&a);
+  printf("enter second number:",b);
+  scanf("%d",&b);
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=a/b;
+  printf("the sum is %d\n",c);
+  printf("the difference is %d\n",d);
+  printf("the product is %d\n",e);
+  printf("the sum is %.2f\n",f);
+  return 0;
+  
+
+
+}
