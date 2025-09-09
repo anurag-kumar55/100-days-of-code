43e8838e04d47f93c5b85e8dd689c9611420f60f
anurag-kumar55 <anuragkumar082005@gmail.com>
2025-09-10 00:40:21 +0530

    Add files via upload

---
+++ DAY6/first.c
@@ -0,0 +1,17 @@
+// Write a program to input an integer and check whether it is even or 
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter an integer:");
+    scanf("%d",&a);
+    
+     if (a %2 == 0 ) 
+          {printf("%d is even\n",a);}
+
+     else{
+        printf("%d is odd\n",a);
+     }
+     return 0;
+}
