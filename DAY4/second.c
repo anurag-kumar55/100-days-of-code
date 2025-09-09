21d6c2b8de6fd1bbcfae93600a01b663e53f92df
anurag-kumar55 <anuragkumar082005@gmail.com>
2025-09-09 23:48:49 +0530

    Updated second.c

--- DAY4/second.c
+++ DAY4/second.c
@@ -1,3 +1,5 @@
+//Write a program to find and display the sum of the first n natural numbers.
+
 #include <stdio.h>
 
 int main() {
@@ -12,4 +14,5 @@ printf("Enter a number: ");
    printf("Sum of first %d natural numbers = %d\n", n, sum);
 
     return 0;
+
 }
