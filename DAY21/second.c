90217e67e8521e861782b117f3dc562f5d2f9ba2
anurag-kumar55 <anuragkumar082005@gmail.com>
2025-11-03 20:26:57 +0530

    Add files via upload

---
+++ DAY21/second.c
@@ -0,0 +1,16 @@
+//WAP to check if a number is perfect number.
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n/2; i++) {
+        if(n % i == 0)
+            sum += i;
+    }
+    if(sum == n)
+        printf("Perfect number");
+    else
+        printf("Not perfect number");   
+    return 0;
+}
