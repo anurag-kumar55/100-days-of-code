626c74d29b311a528f0474ddd100f4f3a8196b9c
anurag-kumar55 <anuragkumar082005@gmail.com>
2025-11-03 13:47:16 +0530

    Add files via upload

---
+++ DAY15/second.c
@@ -0,0 +1,19 @@
+//write a program to reverse a given number.
+#include<stdio.h>
+int main(){
+
+    int num, reverse=0,remainder;
+    printf("Enter a number:");
+    scanf("%d",&num);
+   while (num!=0)
+   {
+    remainder= num % 10;
+reverse= reverse *10+remainder;
+  num= num/10;
+
+   }
+   printf("Reverse number=%d\n",reverse);
+
+   return 0;
+   
+}
