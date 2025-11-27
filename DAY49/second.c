9cbe736ae4904954bfc79cb18033c9de8627ea7e
anurag-kumar55 <anuragkumar082005@gmail.com>
2025-11-27 23:59:55 +0530

    Add files via upload

---
+++ DAY49/second.c
@@ -0,0 +1,43 @@
+// print initials of a name with the surname displayed in full.
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+    char str[1000];
+     printf("Enter String\n");
+    fgets(str,1000,stdin);
+    int n=strlen(str);
+    if(n>0 && (str[n-1]==' '))
+    {
+        str[n-1]='\0';
+        n--;
+    }
+    
+    
+    int i=0,j=n-1;
+
+    while(str[i]==' ')
+    i++;
+    printf("%c.",str[i]);
+    while(str[j]==' ')
+    j--;
+
+    while (str[j]!=' ')
+        j--;
+    
+    while(i<j)
+    {
+        if(str[i]==' ' && (isalpha(str[i+1])))
+        printf("%c.",str[i+1]);
+        i++;
+    }
+    printf(" ");
+    j++;
+    while(str[j]!='\0')
+    printf("%c",str[j++]);
+
+
+    
+    return 0;
+}
